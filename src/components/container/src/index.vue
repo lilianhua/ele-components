ed41b65d1bde9d2f7ff62c155535f43f4a0fc361
lilianhua <850356875@qq.com>
2021-12-23 11:04:29 +0800

    2-4 add container

---
+++ src/components/container/src/index.vue
@@ -0,0 +1,31 @@
+<template>
+      <el-container>
+      <el-aside width='auto'>
+          <nav-sider :collapse='isCollapse'></nav-sider>
+      </el-aside>
+      <el-container>
+        <el-header>
+            <nav-header v-model:collapse="isCollapse"></nav-header>
+        </el-header>
+        <el-main>
+            <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+</template>
+
+<script lang='ts' setup>
+import NavHeader from './navHeader/index.vue'
+import NavSider from './navSide/index.vue'
+import {ref} from 'vue'
+
+let isCollapse = ref(false)
+
+</script>
+
+<style lang='scss' scoped>
+.el-header {
+  padding: 0;
+  border-bottom: 1px solid #eee;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "_vue-router@4.0.12@vue-router";
+import Container from '../components/container/src/index.vue'
 import Home from '../views/Home.vue'
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
-        component: Home
+        component: Container,
+        children: [
+            {
+                path: '/',
+                component: Home
+            }
+        ]
     }
 ]
 
